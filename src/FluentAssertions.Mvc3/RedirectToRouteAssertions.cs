49bb9bfd17e1ccb726445dc51d41a8fedd0b5170
Casey Burns <caseyburns.nz@gmail.com>
2012-04-15 13:04:09 +1200

    create nunit project and statred on RedirectToRoute asserts

--- src/FluentAssertions.Mvc3/RedirectToRouteAssertions.cs
+++ src/FluentAssertions.Mvc3/RedirectToRouteAssertions.cs
@@ -4,14 +4,84 @@ using System.Linq;
 using System.Text;
 using FluentAssertions.Assertions;
 using System.Web.Mvc;
+using FluentAssertions;
 
 namespace FluentAssertions.Mvc3
 {
     public class RedirectToRouteAssertions : ReferenceTypeAssertions<RedirectToRouteResult, RedirectToRouteAssertions>
     {
+        public class Constants
+        {
+            public const string CommonFailMessage = "Expected RedirectResult.{0} to be '{1}' but was '{2}'";
+        }
+
         public RedirectToRouteAssertions(RedirectToRouteResult subject)
         {
             Subject = subject;
+        }
+
+        public RedirectToRouteAssertions WithPermanent(bool expectedPermanent)
+        {
+            WithPermanent(expectedPermanent, string.Empty, null);
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithPermanent(bool expectedPermanent, string reason, params object[] reasonArgs)
+        {
+            Execute.Verification
+                    .ForCondition(expectedPermanent == Subject.Permanent)
+                    .BecauseOf(reason, reasonArgs)
+                    .FailWith(Constants.CommonFailMessage, "Permanent", expectedPermanent, Subject.Permanent);
+
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithRouteName(string expectedRouteName)
+        {
+            WithRouteName(expectedRouteName, string.Empty, null);
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithRouteName(string expectedRouteName, string reason, params object[] reasonArgs)
+        {
+            Execute.Verification
+                    .ForCondition(string.Equals(expectedRouteName, Subject.RouteName, StringComparison.InvariantCultureIgnoreCase))
+                    .BecauseOf(reason, reasonArgs)
+                    .FailWith(Constants.CommonFailMessage, "RouteName", expectedRouteName, Subject.Permanent);
+
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithController(string expectedControllerName)
+        {
+            WithController(expectedControllerName, string.Empty, null);
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithController(string expectedControllerName, string reason, params object[] reasonArgs)
+        {
+            //Subject.RouteValues[""]
+                    
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithRouteValue(string key, object expectedValue)
+        {
+            WithRouteValue(key, expectedValue, string.Empty, null);
+            return this;
+        }
+
+        public RedirectToRouteAssertions WithRouteValue(string key, object expectedValue, string reason, params object[] reasonArgs)
+        {
+            Execute.Verification
+                    .ForCondition(Subject.RouteValues.ContainsKey(key))
+                    .BecauseOf(reason, reasonArgs)
+                    .FailWith(string.Format("RedirectResult.RouteValues does not contain key '{0}'", key));
+            
+            var actualValue = Subject.RouteValues[key];
+            actualValue.Should().Be(expectedValue);
+
+            return this;
         }
     }
 }
---
+++ tests/FluentAssertions.Mvc3.Tests/RedirectToRoute_Tests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Web.Mvc;
+using NUnit.Framework;
+using System.Web.Routing;
+
+namespace FluentAssertions.Mvc3.Tests
+{
+    [TestFixture]
+    public class RedirectToRoute_Tests
+    {
+        [Test]
+		public void WithPermanent_GivenExpected_ShouldPass()
+		{
+            ActionResult result = new RedirectToRouteResult("", null, true);
+            result.Should()
+                    .BeRedirectToRoute()
+                    .WithPermanent(true);
+		}
+
+        [Test]
+        public void WithPermanent_GivenUnxpected_ShouldFail()
+        {
+            ActionResult result = new RedirectToRouteResult("", null, true);
+            Action a = () => result.Should()
+                    .BeRedirectToRoute()
+                    .WithPermanent(false);
+            a.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void WithRouteName_GivenExpected_ShouldPass()
+        {
+            ActionResult result = new RedirectToRouteResult("default", null);
+            result.Should()
+                    .BeRedirectToRoute()
+                    .WithRouteName("default");
+        }
+
+        [Test]
+        public void WithRouteName_GivenUnxpected_ShouldFail()
+        {
+            ActionResult result = new RedirectToRouteResult("default", null);
+            Action a = () => result.Should()
+                    .BeRedirectToRoute()
+                    .WithRouteName("xyz");
+            a.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void WithController_GivenExpected_ShouldPass()
+        {
+            ActionResult result = new RedirectToRouteResult("", new RouteValueDictionary(
+                new {
+                    Controller = "home",
+                    Action = "list",
+                    Id = "22"
+                }));
+
+            result.Should()
+                    .BeRedirectToRoute()
+                    .WithController("Home");
+        }
+
+        [Test]
+        public void WithRouteValue_GivenUnxpected_ShouldFail()
+        {
+            ActionResult result = new RedirectToRouteResult("", new RouteValueDictionary(
+                new
+                {
+                    Controller = "home",
+                    Action = "list",
+                    Id = "22"
+                }));
+
+            Action a = () => result.Should()
+                    .BeRedirectToRoute()
+                    .WithController("xyz");
+            a.ShouldThrow<Exception>();
+        }
+
+
+    }
+}
