1a40dceb367a882bb44531e6873f5ab2740202a1
Kevin Kuszyk <kevin@kevinkuszyk.com>
2013-01-08 14:30:43 +0000

    Version number set to 1.0.0-beta1

--- src/FluentAssertions.Mvc4/Properties/AssemblyInfo.cs
+++ src/FluentAssertions.Mvc4/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("FluentAssertions.Mvc3")]
+[assembly: AssemblyTitle("FluentAssertions.Mvc4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 
