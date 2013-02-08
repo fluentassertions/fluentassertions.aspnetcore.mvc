67d0559589563fd2f6a21967dc3190f45c63f2c5
Kevin Kuszyk <kevin@kevinkuszyk.com>
2013-02-08 21:49:30 +0000

    Version set to 0.1.0.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,5 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0-beta1")]
+[assembly: AssemblyVersion("0.1.0")]
