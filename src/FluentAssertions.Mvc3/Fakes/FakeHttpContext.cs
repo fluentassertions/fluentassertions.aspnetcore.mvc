50e7254cdf6912b3aa4a751243f821a5e654b8b6
James Nail <jrnail23@gmail.com>
2014-01-04 22:13:50 -0500

    Fixed Mvc 5 / Web 4.5 compatibility

--- src/FluentAssertions.Mvc3/Fakes/FakeHttpContext.cs
+++ src/FluentAssertions.Mvc3/Fakes/FakeHttpContext.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Web;
-using System.Web.Hosting;
 
 namespace FluentAssertions.Mvc.Fakes
 {
@@ -37,12 +35,91 @@ namespace FluentAssertions.Mvc.Fakes
             return new FakeHttpWorkerRequest();
         }
 
-        class FakeHttpWorkerRequest : SimpleWorkerRequest
+        class FakeHttpWorkerRequest : HttpWorkerRequest
         {
-            public FakeHttpWorkerRequest()
-                : base(null, null, new StringWriter())
+            public override string GetUriPath()
             {
+                throw new NotImplementedException();
+            }
 
+            public override string GetQueryString()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string GetRawUrl()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string GetHttpVerbName()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string GetHttpVersion()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string GetRemoteAddress()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override int GetRemotePort()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string GetLocalAddress()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override int GetLocalPort()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendStatus(int statusCode, string statusDescription)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendKnownResponseHeader(int index, string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendUnknownResponseHeader(string name, string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendResponseFromMemory(byte[] data, int length)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendResponseFromFile(string filename, long offset, long length)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SendResponseFromFile(IntPtr handle, long offset, long length)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void FlushResponse(bool finalFlush)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void EndOfRequest()
+            {
+                throw new NotImplementedException();
             }
         }
     }
