388cd4273f2a87f646f301cbdf8e7a3a24a48a32
James Nail <jrnail23@gmail.com>
2014-01-04 22:25:36 -0500

    Cleaned up debugging remnants in package script

--- Package.cmd
+++ Package.cmd
@@ -1,17 +1,17 @@
 @ECHO OFF
 pushd %~dp0
 
-REM %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FluentAssertionsMvc.msbuild /m /nr:false /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FluentAssertionsMvc.msbuild /maxcpucount /nodeReuse:false /verbosity:quiet /fileLogger /fileloggerparameters:LogFile=bin\msbuild.log;
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FluentAssertionsMvc.msbuild /m /nr:false /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+
 if errorlevel 1 goto BuildFail
 
 echo.
 echo *** BUILD SUCCESSFUL ***
 echo.
 
-REM .nuget\nuget pack src\FluentAssertions.Mvc3\FluentAssertions.Mvc3.csproj 
-.nuget\nuget pack src\FluentAssertions.Mvc4\FluentAssertions.Mvc4.csproj -verbosity detailed
-.nuget\nuget pack src\FluentAssertions.Mvc5\FluentAssertions.Mvc5.csproj -verbosity detailed
+.nuget\nuget pack src\FluentAssertions.Mvc3\FluentAssertions.Mvc3.csproj 
+.nuget\nuget pack src\FluentAssertions.Mvc4\FluentAssertions.Mvc4.csproj
+.nuget\nuget pack src\FluentAssertions.Mvc5\FluentAssertions.Mvc5.csproj
 if errorlevel 1 goto PackageFail
 
 echo.
