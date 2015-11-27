816b5f617b9d03ba0f9b91daeb33c9d44ec1a58a
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-11-27 16:47:18 +0000

    Move shared code into shared projects

---
+++ src/FluentAssertions.Mvc.Shared/ControllerAssertions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+
+namespace FluentAssertions.Mvc
+{
+    [DebuggerNonUserCode]
+	public class ControllerAssertions
+	{
+		public ControllerAssertions ()
+		{
+		}
+	}
+}
+
