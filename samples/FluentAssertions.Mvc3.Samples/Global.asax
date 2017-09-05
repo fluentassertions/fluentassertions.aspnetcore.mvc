8b2515375cc6dedf054b35fa0d64345c457ee07b
Kevin Kuszyk <kevin@kevinkuszyk.com>
2017-09-05 20:28:39 +0100

    Removing the "classic" mvc projects

--- samples/FluentAssertions.Mvc3.Samples/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FluentAssertions.Mvc.Samples.MvcApplication" Language="C#" %>
--- samples/FluentAssertions.Mvc3.Samples/Models/ProductViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace FluentAssertions.Mvc.Samples.Models
-{
-    public class ProductViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
